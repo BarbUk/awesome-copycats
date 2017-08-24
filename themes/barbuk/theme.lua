e65d3fb6d1c7f0073e7c3834a7a16cc7ae4f036d
BarbUk <julien.virey@gmail.com>
2017-08-24 14:08:38 +0400

    Clean icons

--- themes/barbuk/theme.lua
+++ themes/barbuk/theme.lua
@@ -6,14 +6,14 @@
 
 --]]
 
-local theme                               = {}
+local theme                         = {}
 
 home = os.getenv("HOME")
 
 theme.dir                           = home .. "/.config/awesome/themes/barbuk"
 theme.wallpaper                     = theme.dir .. "/archlinux.png"
 
-theme.font                          = "PragmataPro 7"
+theme.font                          = "PragmataPro 8"
 
 theme.fg_normal                     = "#D8DEE9"
 theme.fg_focus                      = "#EBEBFF"
@@ -26,7 +26,7 @@ theme.border_normal                 = "#0E0E0E"
 theme.border_focus                  = theme.bg_normal
 theme.taglist_fg_focus              = "#EDEFFF"
 theme.taglist_bg_focus              = "#4C566A"
-theme.wibar_height                  = 15
+theme.wibar_height                  = 14
 theme.menu_height                   = 12
 theme.menu_width                    = 140
 theme.menu_border_width             = 0
@@ -41,25 +41,8 @@ theme.tasklist_maximized_horizontal = theme.ocol .. "[M] " .. theme.ccol
 theme.tasklist_maximized_vertical   = ""
 theme.tasklist_disable_icon         = false
 
-theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
-theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
-theme.widget_ac                                 = theme.dir .. "/icons/ac.png"
-theme.widget_battery                            = theme.dir .. "/icons/battery.png"
-theme.widget_battery_low                        = theme.dir .. "/icons/battery_low.png"
-theme.widget_battery_empty                      = theme.dir .. "/icons/battery_empty.png"
-theme.widget_mem                                = theme.dir .. "/icons/mem.png"
-theme.widget_cpu                                = theme.dir .. "/icons/cpu.png"
-theme.widget_temp                               = theme.dir .. "/icons/temp.png"
-theme.widget_net                                = theme.dir .. "/icons/net.png"
-theme.widget_hdd                                = theme.dir .. "/icons/hdd.png"
-theme.widget_music                              = theme.dir .. "/icons/note.png"
-theme.widget_music_on                           = theme.dir .. "/icons/note_on.png"
-theme.widget_vol                                = theme.dir .. "/icons/vol.png"
-theme.widget_vol_low                            = theme.dir .. "/icons/vol_low.png"
-theme.widget_vol_no                             = theme.dir .. "/icons/vol_no.png"
-theme.widget_vol_mute                           = theme.dir .. "/icons/vol_mute.png"
-theme.widget_mail                               = theme.dir .. "/icons/mail.png"
-theme.widget_mail_on                            = theme.dir .. "/icons/mail_on.png"
+theme.taglist_squares_sel           = theme.dir .. "/icons/square_sel.png"
+theme.taglist_squares_unsel         = theme.dir .. "/icons/square_unsel.png"
 
 -- lain related
 theme.layout_txt_termfair           = "[termfair]"
