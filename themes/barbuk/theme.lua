3c530ddc9b89917d88dc9f40cf66b642adf1bbe8
BarbUk <julien.virey@gmail.com>
2017-08-31 00:40:42 +0400

    nord colors

--- themes/barbuk/theme.lua
+++ themes/barbuk/theme.lua
@@ -15,24 +15,41 @@ theme.wallpaper                     = theme.dir .. "/archlinux.png"
 
 theme.font                          = "PragmataPro 8"
 
-theme.fg_normal                     = "#D8DEE9"
-theme.fg_focus                      = "#EBEBFF"
-theme.bg_normal                     = "#2E3440"
+theme.nord0                         = "#2E3440"
+theme.nord1                         = "#3B4252"
+theme.nord2                         = "#434C5E"
+theme.nord3                         = "#4C566A"
+theme.nord4                         = "#D8DEE9"
+theme.nord5                         = "#E5E9F0"
+theme.nord6                         = "#ECEFF4"
+theme.nord7                         = "#8FBCBB"
+theme.nord8                         = "#88C0D0"
+theme.nord9                         = "#81A1C1"
+theme.nord10                        = "#5E81AC"
+theme.nord11                        = "#BF616A"
+theme.nord12                        = "#D08770"
+theme.nord13                        = "#EBCB8B"
+theme.nord14                        = "#A3BE8C"
+theme.nord15                        = "#B48EAD"
+
+theme.fg_normal                     = theme.nord4
+theme.fg_focus                      = theme.nord7
+theme.bg_normal                     = theme.nord0
 theme.bg_focus                      = theme.bg_normal
-theme.fg_urgent                     = "#000000"
-theme.bg_urgent                     = "#A37AB7"
+theme.fg_urgent                     = theme.nord3
+theme.bg_urgent                     = theme.nord15
 theme.border_width                  = 0
-theme.border_normal                 = "#0E0E0E"
+theme.border_normal                 = theme.nord1
 theme.border_focus                  = theme.bg_normal
-theme.taglist_fg_focus              = "#EDEFFF"
-theme.taglist_bg_focus              = "#6795A6"
+theme.taglist_fg_focus              = theme.nord4
+theme.taglist_bg_focus              = theme.nord10
 theme.wibar_height                  = 14
 theme.menu_height                   = 12
 theme.menu_width                    = 140
 theme.menu_border_width             = 0
-theme.useless_gap                   = 0
+theme.useless_gap                   = 1
 
-theme.ocol                          = "<span color='" .. theme.fg_normal .. "'>"
+theme.ocol                          = "<span color='" .. theme.nord8 .. "'>"
 theme.ccol                          = "</span>"
 theme.tasklist_ontop                = theme.ocol .. " " .. theme.ccol
 theme.tasklist_floating             = theme.ocol .. " " .. theme.ccol
